4bef91b05f67b3c8c667907df2e6879eba9c9867
AKyuudere <66967895+AKyuudere@users.noreply.github.com>
2020-08-29 15:58:27 -0400

    Update Ultra Simple Roommate Selector.py

--- Ultra Simple Roommate Selector.py
+++ Ultra Simple Roommate Selector.py
@@ -3,12 +3,12 @@
 
 import json
 
-mem_data = {
+mem_data = {}
 
-}
+# Looking for Roommate or Registering as Potential Roommate
+# Registering adds new user entry into "member_data.json"
 
-#Looking for Roommate or Registering as Potential Roommate
-#Registering adds new user entry into "member_data.json"
+
 def roommate_selection():
     status = 'true'
     print("Welcome to the roommate selection process!")
@@ -26,62 +26,87 @@ def roommate_selection():
             find_process()
 
 
-#Questions that are asked to roommates
+# Questions that are asked to roommates
 def register_process():
     gen_status = 'false'
     hyg_status = 'false'
     smo_status = 'false'
     pet_status = 'false'
     alc_status = 'false'
+    gue_status = 'false'
+    sha_status = 'false'
     a = input("Please enter your name: ")
-    mem_data['name'] = (a)
+    mem_data['name'] = a
     while gen_status == 'false':
         gen = input("What is your gender?: male, female, or other?: ")
         if gen.lower() not in ('male', 'female', 'other'):
             print("Enter 'male', 'female', or 'other'!")
-        else:
+        elif gen == 'female':
             gen_status = 'true'
-            mem_data['gender'] = (gen)
+            mem_data['gender'] = 'femal'
+        elif gen == 'male':
+            gen_status = 'true'
+            mem_data['gender'] = 'male'
+        elif gen == 'other':
+            gen_status = 'true'
+            mem_data['gender'] = 'other'
     while hyg_status == 'false':
         hyg = input("How much do you care about hygiene?: little, average, or lot?: ")
         if hyg.lower() not in ('little', 'average', 'lot'):
             print("Enter 'little', 'average', or 'lot'!")
         else:
             hyg_status = 'true'
-            mem_data['hygiene'] = (hyg)
+            mem_data['hygiene'] = hyg
     while smo_status == 'false':
         smo = input("Your thoughts on smoking: yes or no: ")
         if smo.lower() not in ('yes', 'no'):
             print("Enter 'yes' or 'no'!")
         else:
             smo_status = 'true'
-            mem_data['smoking'] = (smo)
+            mem_data['smoking'] = smo
     while pet_status == 'false':
         pet = input("Are pets allowed?: yes or no: ")
         if pet.lower() not in ('yes', 'no'):
             print("Enter 'yes' or 'no'!")
         else:
             pet_status = 'true'
-            mem_data['pets'] = (pet)
+            mem_data['pets'] = pet
+    while gue_status == 'false':
+        gue = input("Are Guests a yes or no?: ")
+        if gue.lower() not in ('yes', 'no'):
+            print("Enter 'yes' or 'no'!")
+        else:
+            gue_status = 'true'
+            mem_data['guests'] = gue
+    while sha_status == 'false':
+        sha = input("How do you feel about sharing?: yes or no: ")
+        if sha.lower() not in ('yes', 'no'):
+            print("Enter 'yes' or 'no'!")
+        else:
+            sha_status = 'true'
+            mem_data['sharing'] = sha
     while alc_status == 'false':
         alc = input("Thoughts on alcohol: yes or no: ")
         if alc.lower() not in ('yes', 'no'):
             print("Enter 'yes' or 'no'!")
         else:
             alc_status = 'true'
-            mem_data['alcohol'] = (alc)
+            mem_data['alcohol'] = alc
             print(f"Congratulations, Registrations for {a} has been completed!")
             with open("member_data.json") as f:
-                list = json.load(f)
-                list.append(mem_data)
+                lists = json.load(f)
+                lists.append(mem_data)
             with open("member_data.json", 'w') as f:
-                json.dump(list, f, indent=4)
+                json.dump(lists, f, indent=4)
+    ending()
 
 
-#Registered users are searched for in database
+# Registered users are searched for in database
 def find_process():
     global z
     global data
+    global look_name
+    gi_status = 'false'
     status = 'false'
     with open('member_data.json', 'r') as read:
         data = json.load(read)
@@ -92,12 +117,21 @@ def find_process():
             if look_name in z[0]['name']:
                 print("User Found")
                 status = 'true'
-                check_process()
+                while gi_status == 'false':
+                    a = input("Do you care which gender you are put in with?: Yes or No:  ")
+                    if a.lower() not in ('yes', 'no'):
+                        print("Yes or No!")
+                    elif a.lower() == 'yes':
+                        gi_status = 'true'
+                        check_process()
+                    elif a.lower() == 'no':
+                        gi_status = 'true'
+                        check_process_gi()
         except IndexError:
             print("User not found")
 
 
-#Potential Roommates are looked for
+# Potential Roommates are looked for by gender
 def check_process():
     temp_value = []
     potential_rm = []
@@ -110,8 +144,41 @@ def check_process():
         if temp_value[0] in p['gender'] and temp_value[1] in p['hygiene'] and temp_value[2] in p['smoking'] and temp_value[3] in p['pets'] and temp_value[4] in p['alcohol']:
             potential_rm.append(p['name'])
     plength = len(potential_rm)
-    potential_rm.pop(0)
+    potential_rm.remove(f'{look_name}')
     print(f"You have {plength-1} potential roommates, they are: ")
     print(*potential_rm)
+    ending()
+
+
+# Potential Roommate Gender Inclusive
+def check_process_gi():
+    temp_value = []
+    potential_rm = []
+
+    e = z[0]
+    for key in e.keys():
+        temp_value.append(e[key])
+    temp_value.pop(0)
+    for p in data:
+        if temp_value[1] in p['hygiene'] and temp_value[2] in p['smoking'] and temp_value[3] in p['pets'] and temp_value[4] in p['alcohol']:
+            potential_rm.append(p['name'])
+    plength = len(potential_rm)
+    potential_rm.remove(f'{look_name}')
+    print(f"You have {plength-1} potential roommates, they are: ")
+    print(*potential_rm)
+    ending()
+
+
+def ending():
+    print("Do you want to quit?")
+    quits = input("'Yes' to exit or 'No' to go back: ")
+    if quits.lower() not in ("yes", "no"):
+        print("Type yes or no!")
+        ending()
+    elif quits.lower() == 'yes':
+        return
+    elif quits.lower() == 'no':
+        roommate_selection()
+
 
 roommate_selection()
